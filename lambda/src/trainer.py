ce4a273db5234373d118832c270cc2387e28afdc
Trenton Potgieter <darkreapyre@gmail.com>
2018-02-09 14:01:58 -0800

    trainer cleanup: TODO: test actual code

--- lambda/src/trainer.py
+++ lambda/src/trainer.py
@@ -1,62 +1,77 @@
 import time
 import h5py
 import numpy as np
+import os
+from os import environ
+import json
+from json import dumps, loads
+from boto3 import client, resource, Session
+import botocore
+import sys
+
+# Global Variables
+rgn = 'us-east-1'
+s3_client = client('s3', region_name=rgn) # S3 access
+s3_resource = resource('s3')
 
 def load_data():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    # Load main dataset
+    dataset = h5py.File('/tmp/dataset.h5', 'r')
 
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-
+    # Create numpy arrays from the various h5 datasets
+    train_set_x_orig = np.array(dataset["train_set_x"][:]) # train set features
+    train_set_y_orig = np.array(dataset["train_set_y"][:]) # train set labels
+    test_set_x_orig = np.array(dataset["test_set_x"][:]) # test set features
+    test_set_y_orig = np.array(dataset["test_set_y"][:]) # test set labels
+    
+    # Reshape labels
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
 
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig
 
 
-np.random.seed(1)
+def lambda_handler(event, context):
+    # Retrieve datasets and setting from S3
+    input_bucket = s3_resource.Bucket(str(event['Records'][0]['s3']['bucket']['name']))
+    dataset_key = str(event['Records'][0]['s3']['object']['key'])
+    settings_key = dataset_key.split('/')[-2] + '/parameters.json'
+    try:
+        input_bucket.download_file(dataset_key, '/tmp/datasets.h5')
+        input_bucket.download_file(settings_key, '/tmp/parameters.json')
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            sns_message = "Error downloading input data from S3, S3 object does not exist"
+            publish_sns(sns_message)
+        else:
+            raise
+    
+    # Extract the neural network parameters
+    with open('/tmp/parameters.json') as parameters_file:
+        parameters = json.load(parameters_file)
+        
+    np.random.seed(1)
+    train_x_orig, train_y, test_x_orig, test_y = load_data()
 
-train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+    # Reshape the training and test examples
+    train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+    test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 
-# Explore your dataset
-m_train = train_x_orig.shape[0]
-num_px = train_x_orig.shape[1]
-m_test = test_x_orig.shape[0]
+    # Standardize data to have feature values between 0 and 1.
+    train_x = train_x_flatten/255.
+    test_x = test_x_flatten/255.
 
-print ("Number of training examples: " + str(m_train))
-print ("Number of testing examples: " + str(m_test))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_x_orig shape: " + str(train_x_orig.shape))
-print ("train_y shape: " + str(train_y.shape))
-print ("test_x_orig shape: " + str(test_x_orig.shape))
-print ("test_y shape: " + str(test_y.shape))
+    # Debug Statements
+    #print ("train_x's shape: " + str(train_x.shape))
+    #print ("test_x's shape: " + str(test_x.shape))
 
-# Reshape the training and test examples
-train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
-test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+    from DeepNeuralNetwork import DeepNeuralNetwork
+    layers_dims = (12288, 20, 7, 5, 1)
+    activations = ['relu', 'relu', 'relu','sigmoid']
+    num_iter = 10
+    learning_rate = 0.0075
 
-# Standardize data to have feature values between 0 and 1.
-train_x = train_x_flatten/255.
-test_x = test_x_flatten/255.
-
-print ("train_x's shape: " + str(train_x.shape))
-print ("test_x's shape: " + str(test_x.shape))
-# Please note that the above code is from the programming assignment
-
-from DeepNeuralNetwork import DeepNeuralNetwork
-layers_dims = (12288, 20, 7, 5, 1)
-# layers_dims = (12288, 10, 1)
-# layers_dims = [12288, 20, 7, 5, 1] #  5-layer model
-activations = ['relu', 'relu', 'relu','sigmoid']
-num_iter = 2500
-learning_rate = 0.0075
-
-clf = DeepNeuralNetwork(layers_dims, activations)\
-            .fit(train_x, train_y, num_iter, learning_rate, True, 100)
-#print('train accuracy: {:.2f}%'.format(clf.accuracy_score(train_x, train_y)*100))
-print('test accuracy: {:.2f}%'.format(clf.accuracy_score(test_x, test_y)*100))
+    clf = DeepNeuralNetwork(layers_dims, activations)\
+                .fit(train_x, train_y, num_iter, learning_rate, True, 100)
+    #print('train accuracy: {:.2f}%'.format(clf.accuracy_score(train_x, train_y)*100))
+    print('test accuracy: {:.2f}%'.format(clf.accuracy_score(test_x, test_y)*100))
